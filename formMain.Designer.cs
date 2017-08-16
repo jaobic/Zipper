16486aa3d1b7e9e3e3531cf768ac34a169417bdf
Patrick Bateman <patrickb8man@users.noreply.github.com>
2017-08-17 01:39:09 +1000

    Initial commit

---
+++ formMain.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Zipper
+{
+    partial class formMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formMain));
+            this.lsvFiles = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.imgIcons = new System.Windows.Forms.ImageList(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnNew = new System.Windows.Forms.ToolStripButton();
+            this.btnOpen = new System.Windows.Forms.ToolStripButton();
+            this.btnCompress = new System.Windows.Forms.ToolStripButton();
+            this.btnExtractAll = new System.Windows.Forms.ToolStripButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lsvFiles
+            // 
+            this.lsvFiles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.lsvFiles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lsvFiles.GridLines = true;
+            this.lsvFiles.Location = new System.Drawing.Point(0, 45);
+            this.lsvFiles.Name = "lsvFiles";
+            this.lsvFiles.Size = new System.Drawing.Size(612, 216);
+            this.lsvFiles.SmallImageList = this.imgIcons;
+            this.lsvFiles.TabIndex = 4;
+            this.lsvFiles.UseCompatibleStateImageBehavior = false;
+            this.lsvFiles.View = System.Windows.Forms.View.Details;
+            this.lsvFiles.DoubleClick += new System.EventHandler(this.lsvFiles_DoubleClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Name";
+            this.columnHeader1.Width = 282;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Size";
+            this.columnHeader2.Width = 108;
+            // 
+            // imgIcons
+            // 
+            this.imgIcons.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgIcons.ImageStream")));
+            this.imgIcons.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgIcons.Images.SetKeyName(0, "document.png");
+            this.imgIcons.Images.SetKeyName(1, "folder.png");
+            this.imgIcons.Images.SetKeyName(2, "document-music.png");
+            this.imgIcons.Images.SetKeyName(3, "document-image.png");
+            this.imgIcons.Images.SetKeyName(4, "document-film.png");
+            this.imgIcons.Images.SetKeyName(5, "document-zipper.png");
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnNew,
+            this.btnOpen,
+            this.btnCompress,
+            this.btnExtractAll});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(612, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Image = ((System.Drawing.Image)(resources.GetObject("btnNew.Image")));
+            this.btnNew.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(51, 22);
+            this.btnNew.Text = "New";
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Image = ((System.Drawing.Image)(resources.GetObject("btnOpen.Image")));
+            this.btnOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(56, 22);
+            this.btnOpen.Text = "Open";
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnCompress
+            // 
+            this.btnCompress.Enabled = false;
+            this.btnCompress.Image = ((System.Drawing.Image)(resources.GetObject("btnCompress.Image")));
+            this.btnCompress.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCompress.Name = "btnCompress";
+            this.btnCompress.Size = new System.Drawing.Size(80, 22);
+            this.btnCompress.Text = "Compress";
+            // 
+            // btnExtractAll
+            // 
+            this.btnExtractAll.Image = ((System.Drawing.Image)(resources.GetObject("btnExtractAll.Image")));
+            this.btnExtractAll.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnExtractAll.Name = "btnExtractAll";
+            this.btnExtractAll.Size = new System.Drawing.Size(79, 22);
+            this.btnExtractAll.Text = "Extract All";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.textBox1.Location = new System.Drawing.Point(0, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(612, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // formMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(612, 261);
+            this.Controls.Add(this.lsvFiles);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "formMain";
+            this.Text = "Zipper";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lsvFiles;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnNew;
+        private System.Windows.Forms.ToolStripButton btnOpen;
+        private System.Windows.Forms.ToolStripButton btnCompress;
+        private System.Windows.Forms.ToolStripButton btnExtractAll;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ImageList imgIcons;
+    }
+}
---
+++ formMain.cs
@@ -0,0 +1,138 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zipper
+{
+    public partial class formMain : Form
+    {
+        public formMain()
+        {
+            InitializeComponent();
+        }
+
+        public ZipFile file;
+        public List<ZipEntry> entries;
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog()
+            {
+                Filter = "Zip compressed folders (*.zip)|*.zip"
+            };
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                file = new ZipFile(open.FileName);
+                entries = file.Cast<ZipEntry>().ToList();
+
+                ReloadEntries("/");
+            }
+        }
+
+        public ICollection<ZipEntry> GetEntries(string path)
+        {
+            List<ZipEntry> pathEntries = new List<ZipEntry>();
+            string correctPath = path.TrimStart('/');
+
+            foreach (var entry in entries)
+            {
+                if (entry.Name.Contains(correctPath) &&
+                    entry.Name.TrimEnd('/').LastIndexOf('/') == correctPath.LastIndexOf('/'))
+                    pathEntries.Add(entry);
+            }
+
+            return pathEntries;
+        }
+
+        public void ReloadEntries(string path)
+        {
+            lsvFiles.Items.Clear();
+
+            if (path != "/")
+            {
+                var root = lsvFiles.Items.Add((".."));
+                root.ImageIndex = root.StateImageIndex = 1;
+
+                root.Tag = new EntryMetadata
+                {
+                    IsFolder = true,
+                    Path = path.Remove(path.TrimEnd('/').LastIndexOf('/') + 1)
+                };
+            }
+
+            foreach (var entry in GetEntries(path))
+            {
+                string filename = entry.Name.Trim('/');
+                filename = filename.Remove(0, filename.LastIndexOf('/') + 1);
+
+                var item = lsvFiles.Items.Add(filename);
+
+                if (entry.IsFile)
+                    item.SubItems.Add(Utility.GetBytesReadable(entry.Size).ToString());
+
+                if (!entry.IsFile)
+                    item.ImageIndex = item.StateImageIndex = 1;
+                else if (entry.Name.EndsWith(".wav")
+                    || entry.Name.EndsWith(".mp3")
+                    || entry.Name.EndsWith(".flac")
+                    || entry.Name.EndsWith(".aiff")
+                    || entry.Name.EndsWith(".wma"))
+                    item.ImageIndex = item.StateImageIndex = 2;
+                else if (entry.Name.EndsWith(".bmp")
+                    || entry.Name.EndsWith(".jpg")
+                    || entry.Name.EndsWith(".jpeg")
+                    || entry.Name.EndsWith(".png")
+                    || entry.Name.EndsWith(".gif")
+                    || entry.Name.EndsWith(".tiff"))
+                    item.ImageIndex = item.StateImageIndex = 3;
+                else if (entry.Name.EndsWith(".mp4")
+                    || entry.Name.EndsWith(".mkv")
+                    || entry.Name.EndsWith(".webm"))
+                    item.ImageIndex = item.StateImageIndex = 4;
+                else if (entry.Name.EndsWith(".zip")
+                    || entry.Name.EndsWith(".7z")
+                    || entry.Name.EndsWith(".gz")
+                    || entry.Name.EndsWith(".rar"))
+                    item.ImageIndex = item.StateImageIndex = 5;
+                else
+                    item.ImageIndex = item.StateImageIndex = 0;
+
+                item.Tag = new EntryMetadata
+                {
+                    IsFolder = !entry.IsFile,
+                    Path = "/" + entry.Name
+                };
+            }
+        }
+
+        private void lsvFiles_DoubleClick(object sender, EventArgs e)
+        {
+            if (lsvFiles.SelectedIndices.Count > 0)
+            {
+                EntryMetadata meta = lsvFiles.SelectedItems[0].Tag as EntryMetadata;
+
+                if (meta.IsFolder)
+                    ReloadEntries(meta.Path);
+            }
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            lsvFiles.Items.Clear();
+        }
+    }
+
+    public class EntryMetadata
+    {
+        public bool IsFolder;
+        public string Path;
+    }
+}
